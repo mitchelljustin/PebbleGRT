a1410e6e4d72706ae9155d9e4af34980ce38f08a
Mitchell Van Der Hoeff <mitchell.vanderhoeff@gmail.com>
2014-11-16 18:06:02 -0500

    First successful compile

--- src/main.c
+++ src/main.c
@@ -1,19 +1,36 @@
 #include <pebble.h>
 
+#define MAX_NUM_MENU_ITEMS 10
+
+static SimpleMenuItem s_menu_items[MAX_NUM_MENU_ITEMS];  
+
+#define KEY_TYPE 0
+
 static Window *s_main_window;
 static SimpleMenuLayer *s_nearby_buses_layer;
+static SimpleMenuSection s_default_menu_section = {
+  .items = s_menu_items,
+  .num_items = 1,
+  .title = "Nearby Buses"
+};
 
 static void main_window_load(Window *window) {
-  s_nearby_buses_layer = simple_menu_layer_create 	(GRect(0,0,0
-		Window *  	window,
-		const SimpleMenuSection *  	sections,
-		int32_t  	num_sections,
-		void *  	callback_context
-	)
+  s_menu_items[0] = (SimpleMenuItem) {
+    .title = "Loading.."
+  };
+  
+  Layer *window_layer = window_get_root_layer(window);
+  GRect window_bounds = layer_get_bounds(window_layer);
+  s_nearby_buses_layer = simple_menu_layer_create(window_bounds,
+                                                  window,
+                                                  &s_default_menu_section,
+                                                  1,
+                                                  NULL);
+  layer_add_child(window_layer, simple_menu_layer_get_layer(s_nearby_buses_layer));
 }
 
 static void main_window_unload(Window *window) {
-
+  simple_menu_layer_destroy(s_nearby_buses_layer);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -22,6 +39,37 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
   // TODO: parse bus data
+    // Get the first pair
+  Tuple *t = dict_read_first(iterator);
+
+  int index = 0;
+  // Process all pairs present
+  while(t != NULL) {
+    // Process this pair's key
+    switch (t->key) {
+      case KEY_TYPE:
+        APP_LOG(APP_LOG_LEVEL_INFO, "KEY_TYPE received with value %d", (int)t->value->int32);
+        break;
+      default: {
+        const char *bus_string = t->value->cstring;
+        SimpleMenuItem bus_item = {
+          .title = bus_string,
+          .subtitle = NULL,
+          .icon = NULL,
+          .callback = NULL
+        };
+        s_menu_items[index] = bus_item;
+        index += 1;
+        break;
+      }
+    }
+
+    // Get next pair, if any
+    t = dict_read_next(iterator);
+  }
+  
+  s_default_menu_section.num_items = index + 1;
+  layer_mark_dirty(simple_menu_layer_get_layer(s_nearby_buses_layer));
 }
 
 static void inbox_dropped_callback(AppMessageResult reason, void *context) {
