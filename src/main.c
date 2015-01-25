d099fd206b680d50e1c4b9e04f4bfa309fd3fd00
Mitchell Vanderhoeff <mitchellvanderhoeff@gmail.com>
2015-01-24 21:44:47 -0500

    cleanup of main

--- src/main.c
+++ src/main.c
@@ -1,67 +1,60 @@
 #include <pebble.h>
 
-static Window *s_main_window;
+static struct {
+   SimpleMenuLayer *menu_layer;
+   SimpleMenuSection actions_menu_section;
+   SimpleMenuItem actions_menu_items[2];
+   Window *window;
+} S;
 
-static SimpleMenuItem s_main_menu_items[1];
+static void main_window_load(Window *window) {
+   Layer *window_layer = window_get_root_layer(window);
+   GRect window_bounds = layer_get_bounds(window_layer);
+   S.menu_layer = simple_menu_layer_create(window_bounds,
+      window,
+      &S.actions_menu_section,
+      1,
+      NULL);
 
-static SimpleMenuSection s_main_menu_section = {
-    .items = s_main_menu_items,
-    .num_items = 1,
-    .title = "Actions"
-};
-
-static SimpleMenuLayer *s_main_menu_layer;
-
-static void main_window_load(Window *window)
-{
-    Layer *window_layer = window_get_root_layer(window);
-    GRect window_bounds = layer_get_bounds(window_layer);
-    s_main_menu_layer = simple_menu_layer_create(window_bounds,
-        window,
-        &s_main_menu_section,
-        1,
-        NULL);
-
-    layer_add_child(window_layer, simple_menu_layer_get_layer(s_main_menu_layer));
+   layer_add_child(window_layer, simple_menu_layer_get_layer(S.menu_layer));
 }
 
-static void main_window_unload(Window *window)
-{
-    simple_menu_layer_destroy(s_main_menu_layer);
+static void main_window_unload(Window *window) {
+   simple_menu_layer_destroy(S.menu_layer);
 }
 
-static void setup_main_menu_items()
-{
-    s_main_menu_items[0] = (SimpleMenuItem) {
-        .title = "Nearby buses"
-    };
+static void init() {
+   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+
+   S.actions_menu_items[0] = (SimpleMenuItem) {
+      .title = "Nearby Buses"
+   };
+   S.actions_menu_items[1] = (SimpleMenuItem) {
+      .title = "Nearby Stops"
+   };
+   S.actions_menu_section = (SimpleMenuSection) {
+      .num_items = 2,
+      .items = S.actions_menu_items,
+      .title = "PebbleGRT"
+   };
+
+   S.window = window_create();
+
+   window_set_window_handlers(S.window, (WindowHandlers) {
+      .load = main_window_load,
+      .unload = main_window_unload
+   });
+
+   window_stack_push(S.window, true);
 }
 
-static void init()
-{
-    app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
-
-    setup_main_menu_items();
-
-    s_main_window = window_create();
-
-    window_set_window_handlers(s_main_window, (WindowHandlers) {
-        .load = main_window_load,
-        .unload = main_window_unload
-    });
-
-    window_stack_push(s_main_window, true);
+static void deinit() {
+   window_destroy(S.window);
 }
 
-static void deinit()
-{
-    window_destroy(s_main_window);
-}
-
-int main(void)
-{
-    init();
-    app_event_loop();
-    deinit();
-    return 0;
+int main(void) {
+   init();
+   app_event_loop();
+   deinit();
+   return 0;
 }
